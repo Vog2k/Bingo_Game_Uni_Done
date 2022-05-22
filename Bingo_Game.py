93674cc33e245335ef1b085e63ea2cd820ce95fc
Vog2k <99256921+Vog2k@users.noreply.github.com>
2022-05-23 08:44:47 +1200

    Almost ready

--- Bingo_Game.py
+++ Bingo_Game.py
@@ -1,40 +1,13 @@
 import random
 import copy
-"""I definatly had help from google/Youtube with this one as it was super hard for me"""
-"""
-Welcome = input("Welcome to My Bingo game, Press enter to continue!:")
-print()
-print("Please read terms and conditions before playing.")
-print()  # Since bingo is a form of gambling i thought i would add a terms and conditions.
-print("We here at M.B.G do not condone gambling By playing Bingo\nyou agree to be bound by these terms and "
-      "conditions but they do not\naffect your rights under the Consumer Rights Act 2015. ")
-print(
-    "You must be over 18\nyears of age to play My Bingo Game reserves the right to require all\nparticipants to prove "
-    "that they are over 18 by production of a\n recognised photographic identity document.")
-print()
-# Little login phase
-# Asks the user to enter their age
-AGE = int(input("Please enter your Age:"))
-if AGE > 17:  # If the age is over 17 then the user will be eligible if not then the programme will end
-    print("You are Eligible ")
-else:
-    print("Sorry you are not Eligible to play")
-    exit()
-
-Go = input("Press Enter to accept and continue:")
-print()
-print("Welcome to My Bingo Game")
-print()
-Enter = input("Press Enter to start a game")"""
 
 
 def clearScreen():
-    print(chr(27) + "[2J")
+    print(chr(27) + "[2J")  # Escape sequence to clear screen
 
 
 def generateBoard():
-    # This will be the random number generator for your board
-    whitelist = list(range(1, 80))
+    whitelist = list(range(1, 81))
     board = [list(), list(), list(), list(), list()]
     for x in range(0, 5):
         for y in range(0, 5):
@@ -47,7 +20,6 @@ def generateBoard():
 
 
 # checks board if contains num
-# Type in your number
 def inBoard(num, board):
     for x in range(0, 5):
         for y in range(0, 5):
@@ -91,7 +63,7 @@ def didWin(board):
     if count == 5:
         return True
 
-    # check for diagonal - top right to bottom left
+    # check for diagnal - top right to bottom left
     count = 0
     for x in range(0, 5):
         if board[4 - x][x] != ' X':
@@ -105,8 +77,7 @@ def didWin(board):
 
 
 def printBoard(board, OG):
-    print("     Here is your board")
-    print("Marked board   |   Unmarked board\n")
+    print(' Marked Board   |  Board Numbers')
     for y in range(0, 5):
         ogStr = ''
         for x in range(0, 5):
@@ -120,40 +91,40 @@ def printBoard(board, OG):
 
 
 def draw(notDrawn):
-    num = notDrawn(range(1, 80))
-    notDrawn.remove(num)
-    #num = notDrawn[random.randint(0, len(notDrawn) - 1)]
-    #notDrawn.remove(num)
-    return num
+    rannum = int(input("Please enter a number between 1-80: "))
+    print()
+    notDrawn.remove(rannum)
+    return rannum
+
+
 def playGame():
-    notDrawn = (range(1, 80))
+    notDrawn = list(range(1, 81))
     board1 = generateBoard()
     OG_BOARD1 = copy.deepcopy(board1)
 
     clearScreen()
-    print('Enter Draw Number OR Enter q, then press enter to quit.\n')
-
+    print('Enter q, then press enter to quit.\n')
     printBoard(board1, OG_BOARD1)
 
     while True:
-        userInput = input("Enter number :")
+        userInput = input
         clearScreen()
         if userInput == 'q':
-            print('Press Enter to Draw Number. Enter q, then press enter to quit.\n')
+            print('Enter q, then press enter to quit.\n')
             printBoard(board1, OG_BOARD1)
             print("Game Over")
             exit()
         else:
             curDraw = draw(notDrawn)
             found = inBoard(curDraw, board1)
-            print('Press Enter to Draw Number. Enter q, then press enter to quit.\n')
+            print('Enter q, then press enter to quit.\n')
             printBoard(board1, OG_BOARD1)
             if didWin(board1) == True:
                 while True:
                     clearScreen()
                     print('Winner! Enter "new" to start new game. Enter "q" to quit.\n')
                     printBoard(board1, OG_BOARD1)
-                    print('BINGO!!! --- YOU WIN! --- Drew ' + str(curDraw))
+                    print('BINGO!!! Congratulations you have won, Last drawn number' + str(curDraw))
                     userInput = input()
                     if userInput == 'q':
                         print("Game Over")
@@ -162,10 +133,13 @@ def playGame():
                         return True
             else:
                 if found == True:
-                    print("Found a match! " + str(curDraw))
+                    print()
+                    print("Found a match!\n\nYou're number " + str(curDraw))
+                    print()
                 else:
                     print()
-                    print("No match " + str(curDraw))
+                    print("Bad draw. No Match.\n\nYou're number " + str(curDraw))
+                    print()
 
 
 runGame = True
